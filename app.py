3a27a3269b64ba2086d3ad14c77782c8aa7864ba
Artur <artur_goncharov_97@mail.ru>
2022-11-13 22:06:59 +0300

    save messages history

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import asyncio
 from typing import NoReturn
+from datetime import datetime
+
+import aiofiles
 
 
-async def echo_client(ip: str, port: int) -> NoReturn:
-    """Установка соединения с чатом"""
+async def read_chat(ip: str, port: int) -> NoReturn:
+    """Чтения переписки чата"""
+    
     reader, writer = await asyncio.open_connection(ip, port)
+    datetime_at = datetime.now().strftime(r'%Y-%m-%d %H:%M:%S')
     while True:
-        message = await reader.readline()
-        print(message.decode())
+        data = await reader.readline()
+        message = f'[{datetime_at}] {data.decode()}'
+        print(message)
+        await save_messages_history(message)
+
+
+async def save_messages_history(message:str)->NoReturn:
+    """Записывает сообщения в файл"""
+
+    async with aiofiles.open('messages_history.log', 'a') as history_file:
+        await history_file.write(message)
 
 
 def main():
     ip = 'minechat.dvmn.org'
     port = 5000
-    asyncio.run(echo_client(ip, port))
+    asyncio.run(read_chat(ip, port))
 
 
 if __name__ == "__main__":
