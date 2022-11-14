2d3b9fdd4a37b63e94c57446148ceebbde018d75
Artur <artur_goncharov_97@mail.ru>
2022-11-14 23:42:14 +0300

    Add new script to send messages

---
+++ send_message.py
@@ -0,0 +1,45 @@
+import asyncio
+from typing import NoReturn
+
+from cli import CLI
+from connect import connection_chat
+
+
+async def authentication(reader:asyncio.StreamReader, writer:asyncio.StreamWriter, user_token:str) -> str:
+    """Аутентификация пользователя по токену"""
+    
+    await reader.readline()
+    writer.write(f'{user_token}\n'.encode())
+    await writer.drain()
+    data = await reader.readline()
+    print(f"Welcome to chat! \n{data.decode()}")
+    return data.decode()
+    
+
+async def send_message(writer:asyncio.StreamWriter, reader:asyncio.StreamReader)-> NoReturn:
+    """Отправка нового сообщения"""
+
+    await reader.readline()
+    while True:
+        message = input('Enter your message: ')
+        writer.write(f'{message}\n\n'.encode())
+        await writer.drain()
+
+        data = await reader.readline()
+        print(data.decode())
+
+
+async def main():
+    cli = CLI()
+    args = cli.parser.parse_args()
+
+    user_token = args.token
+    reader, writer = await connection_chat(args.host, args.port)
+
+    if user_token is not None:
+        await authentication(reader, writer, user_token)
+    await send_message(writer,reader)
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
