de882a56117315339e41e2b3cda34aa4be7ab35f
goncharov-a <goncharov-a@gaz-is.ru>
2022-11-21 23:21:07 +0300

    added defalt value

--- cli.py
+++ cli.py
@@ -10,8 +10,8 @@ class CLI():
             description='Underground chat'
         )
 
-        self.parser.add_argument('--host', required=True, help="Chat Ip address")
-        self.parser.add_argument('--port', required=True, help="Chat port")
+        self.parser.add_argument('--host', required=True, default='chat.org', help="Chat Ip address")
+        self.parser.add_argument('--port', default=5000, help="Chat port")
 
         self.parser.add_argument('--username', help="Registrational of new user")
         self.parser.add_argument('--token', help="User token")
